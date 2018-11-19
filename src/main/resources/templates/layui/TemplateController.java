3e2c8fb649677400abe77028dacf101361b08808
Wu Bing <961261955@qq.com>
2018-11-19 16:20:15 +0800

    modify controller

--- src/main/resources/templates/layui/TemplateController.java
+++ src/main/resources/templates/layui/TemplateController.java
@@ -21,40 +21,4 @@ public class ${javaAttribute.controller.name} extends BaseControllerImpl<${javaA
 	public ${javaAttribute.controller.name}(@Qualifier("${javaAttribute.service.parametterName}") AbstractService service) {
 		super(service);
 	}
-	/**
-	 * 分页查询列表数据
-	 */
-	@RequestMapping("/query")
-	public Page<${javaAttribute.model.name}> query(@Validated @ModelAttribute RequestPage requestPage, @Validated @ModelAttribute ${javaAttribute.model.name} ${javaAttribute.model.parametterName}) {
-		return super.query(requestPage, ${javaAttribute.model.parametterName});
-	}
-	/**
-	 * 根据主键查询数据
-	 */
-	@RequestMapping("/get")
-	public ${javaAttribute.model.name} get(@RequestParam(value="${javaAttribute.primaryKey}") ${javaAttribute.primaryKeyType} ${javaAttribute.primaryKey} ) {
-			return super.get(${javaAttribute.primaryKey});
-	}
-	/**
-	 * 根据主键删除数据
-	 */
-	@RequestMapping("/delete")
-	public ResponseResult delete(@RequestParam(value="${javaAttribute.primaryKey}") ${javaAttribute.primaryKey} ) {
-			return super.delete(${javaAttribute.primaryKey});
-	}
-
-	/**
-	 *保存操作
-	 */
-	@RequestMapping(value = "/save")
-	public ResponseResult save(@Validated @ModelAttribute ${javaAttribute.model.name} ${javaAttribute.model.parametterName}){
-		ResponseResult responseResult;
-		if(${javaAttribute.model.parametterName}.getId()==null||"".equals(${javaAttribute.model.parametterName}.getId())){
-			${javaAttribute.model.parametterName}.setId(UUIDutil.UUID());
-			responseResult = super.create(${javaAttribute.model.parametterName});
-		}else{
-			responseResult = super.update(${javaAttribute.model.parametterName});
-		}
-		return responseResult;
-	}
 }
